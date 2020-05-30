a49478048efb8258a61bdd453fd3b15126d993c4
abdul mudhir <abdulmudhirss@gmail.com>
2020-05-30 14:58:15 +0100

    fixed issue where it would add duplicate firewall to inbound rule

--- firewall_online.py
+++ firewall_online.py
@@ -19,8 +19,20 @@ def firewall_exist():
     return "No rules match the specified criteria." == output_message.decode().strip()
 
 
+def firewall_scopes_list():
+
+    if firewall_exist():
+
+        netsh_firewall_exist = f'''netsh advfirewall firewall show rule name="{firewall_rule_name}"'''
+        in_command = Popen(netsh_firewall_exist, stdout=PIPE, stderr=PIPE)
+
+        output_message, _ = in_command.communicate()
+
+
+
+
 def add_firewall_rule():
-    netsh_add_firewall_command_in = f'''netsh advfirewall firewall add rule name="{firewall_rule_name}" dir=in action=block program="{program_path}" enable=no profile=domain,private,public protocol=UDP localport=6672'''
+    netsh_add_firewall_command_in = f'''netsh advfirewall firewall add rule name="{firewall_rule_name}" dir=in action=block program="{program_path}" enable=no profile=domain,private,public protocol=UDP localport=6672  '''
     netsh_add_firewall_command_out = f'''netsh advfirewall firewall add rule name="{firewall_rule_name}" dir=out action=block program="{program_path}" enable=no profile=domain,private,public protocol=UDP localport=6672'''
     in_command = Popen(netsh_add_firewall_command_in, stdout=PIPE, stderr=PIPE)
     out_command = Popen(netsh_add_firewall_command_out, stdout=PIPE, stderr=PIPE)
@@ -30,11 +42,10 @@ def add_firewall_rule():
     return "Ok." == output_message.decode().strip()
 
 
-add_firewall_rule()
 
 
 def add_white_list(ip_address):
-    netsh_allow_remote_address = f'''netsh advfirewall firewall set rule name="{firewall_rule_name}" new remoteip={ip_address}'''
+    netsh_allow_remote_address = f'''netsh advfirewall firewall set rule name="{firewall_rule_name}" dir=in new remoteip={ip_address} '''
     call(netsh_allow_remote_address)
 
 
@@ -54,3 +65,5 @@ def delete_firewall_rule():
 
 
 delete_firewall_rule()
+add_firewall_rule()
+add_white_list("192.168.1.1")
