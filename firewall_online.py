e08d5d991e4be45da4279a2d410e36c5b9da8f90
abdul mudhir <abdulmudhirss@gmail.com>
2020-05-30 15:25:45 +0100

    retrieve the remote ip address

--- firewall_online.py
+++ firewall_online.py
@@ -16,19 +16,24 @@ def firewall_exist():
 
     output_message, _ = in_command.communicate()
 
-    return "No rules match the specified criteria." == output_message.decode().strip()
+    return "No rules match the specified criteria." != output_message.decode().strip()
 
 
 def firewall_scopes_list():
 
     if firewall_exist():
 
-        netsh_firewall_exist = f'''netsh advfirewall firewall show rule name="{firewall_rule_name}"'''
+        netsh_firewall_exist = f'''netsh advfirewall firewall show rule name="{firewall_rule_name}" dir=in '''
         in_command = Popen(netsh_firewall_exist, stdout=PIPE, stderr=PIPE)
 
         output_message, _ = in_command.communicate()
 
+        output_message_decoded = output_message.decode().strip()
 
+        remote_ip_address = output_message_decoded.split()[17]
+
+
+        print(remote_ip_address)
 
 
 def add_firewall_rule():
@@ -42,8 +47,6 @@ def add_firewall_rule():
     return "Ok." == output_message.decode().strip()
 
 
-
-
 def add_white_list(ip_address):
     netsh_allow_remote_address = f'''netsh advfirewall firewall set rule name="{firewall_rule_name}" dir=in new remoteip={ip_address} '''
     call(netsh_allow_remote_address)
@@ -54,7 +57,7 @@ def enable_firewall_rule():
     call(netsh_add_firewall_command)
 
 
-def disabl_firewall_rule():
+def disable_firewall_rule():
     netsh_add_firewall_command = f'''netsh advfirewall firewall set rule name="{firewall_rule_name}" enable=no '''
     call(netsh_add_firewall_command)
 
@@ -64,6 +67,6 @@ def delete_firewall_rule():
     call(netsh_add_firewall_command)
 
 
-delete_firewall_rule()
-add_firewall_rule()
-add_white_list("192.168.1.1")
+# delete_firewall_rule()
+# add_firewall_rule()
+firewall_scopes_list()
