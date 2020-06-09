c9345f4c9387b9642e8817a23ad7bed2ee4266ed
abdul mudhir <abdulmudhirss@gmail.com>
2020-06-09 18:44:44 +0100

    Thread created for hot key

--- hotkey.py
+++ hotkey.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QGridLayout
 from PyQt5 import QtWidgets , QtGui
+from PyQt5.QtCore import QThread
 
 
 
@@ -26,6 +27,7 @@ class HotKey(QDialog):
         firewall_on_hot_key = QLabel("Firewall On")
         firewall_on_hot_key.setStyleSheet("border:0.5px solid black;")
 
+
         firewall_off_hot_key = QLabel("Firewall Off")
         firewall_off_hot_key.setStyleSheet("border:1px solid black;")
 
@@ -36,11 +38,13 @@ class HotKey(QDialog):
         resource_monitor_button.setText("F10")
 
 
-        firewall_on_button = QPushButton(self)
-        firewall_on_button.setText("F11")
+        self.firewall_on_button = QPushButton(self)
+        self.firewall_on_button.setText("F11")
+        self.firewall_on_button.clicked.connect(self.test)
+        self.firewall_on_button.setFixedSize(100, 23)
 
-        firewall_off_button = QPushButton(self)
-        firewall_off_button.setText("F12")
+        self.firewall_off_button = QPushButton(self)
+        self.firewall_off_button.setText("F12")
 
 
         option_label = QLabel("Options")
@@ -48,23 +52,51 @@ class HotKey(QDialog):
         # row and column
         main_layout.addWidget(option_label, 0, 0)
         main_layout.addWidget(hot_key_label, 0, 1)
-        main_layout.addWidget(firewall_on_button, 1,1)
+        main_layout.addWidget(self.firewall_on_button, 1,1)
         main_layout.addWidget(firewall_on_hot_key,1,0 )
 
         main_layout.addWidget(firewall_off_hot_key, 2, 0)
-        main_layout.addWidget(firewall_off_button, 2, 1)
+        main_layout.addWidget(self.firewall_off_button, 2, 1)
 
         main_layout.addWidget(resource_monitor_button, 3, 1)
         main_layout.addWidget(resource_monitor_key,  3, 0)
 
         main_layout.setHorizontalSpacing(10)
 
-        main_layout.setColumnStretch(0,1)
+        main_layout.setColumnStretch(0,2)
 
-        main_layout.addWidget(firewall_on_button)
 
 
         self.setLayout(main_layout)
+
+    def test(self):
+        self.firewall_on_button.setStyleSheet("background-color:yellow;border:0.5px solid black;")
+        self.firewall_on_button.setText("Select a hotkey...")
+        test = InputCheck(self)
+        test.start()
+
+
+
+
+    def keyPressEvent(self, key):
+        pass
+
+
+class InputCheck(QThread):
+
+    def __init__(self, parent =None):
+        super().__init__(parent)
+
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        key_stroke_found = False
+
+        while not key_stroke_found:
+            print("1")
+
 
 
 
